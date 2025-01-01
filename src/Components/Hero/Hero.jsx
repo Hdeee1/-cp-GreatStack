a87651776bb3f03bfd5187a000d379706cea7dde
Hadyu <hadyuhakim@gmail.com>
2025-01-01 17:34:22 +0700

    setup, create components navbar & hero

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Hero.css";
+import dark_arrow from "../../assets/dark-arrow.png";
+
+const Hero = () => {
+  return (
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. At illum
+          ullam sunt sapiente, sint modi maiores quidem nesciunt dignissimos
+          nemo ut, aspernatur repudiandae libero delectus labore omnis
+          doloremque. Maiores, dignissimos!. Lorem, ipsum dolor.
+        </p>
+        <button className="btn">
+          Explore More <img src={dark_arrow} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
