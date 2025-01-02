966b8338b05104351f9ac66ea3fa28f8aafe7cdb
Hadyu <hadyuhakim@gmail.com>
2025-01-02 14:04:06 +0700

    create campus components, modified the apps.jsx, index.css

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
 import Title from "./Components/Title/Title";
 import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
 
 const App = () => {
   return (
@@ -16,6 +17,8 @@ const App = () => {
         <Programs />
         <About />
         <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
+        <Title subTitle="TESTIMONIAL" title="What Students Says" />
       </div>
     </div>
   );
