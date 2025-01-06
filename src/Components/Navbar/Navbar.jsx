25a92f4f5a1176dfe547e0140254c0a9cf0f4847
Hadyu <hadyuhakim@gmail.com>
2025-01-06 08:12:10 +0700

    create responsive navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect } from "react";
 import "./Navbar.css";
 import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [sticky, setSticky] = React.useState(false);
@@ -16,13 +17,41 @@ const Navbar = () => {
     <nav className={`container ${sticky ? "dark-nav" : ""}`}>
       <img src={logo} alt="" className="logo" />
       <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About Us</li>
-        <li>Campus</li>
-        <li>Testimonial</li>
         <li>
-          <button className="btn">Contact Us</button>
+          <Link to="hero" smooth={true} offset={0} duration={500} className='hvr'>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="program" smooth={true} offset={-260} duration={500} className='hvr'>
+            Program
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} offset={-150} duration={500} className='hvr'>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="campus" smooth={true} offset={-260} duration={500} className='hvr'>
+            Campus
+          </Link>
+        </li>
+        <li>
+          <Link to="testimonial" smooth={true} offset={-260} duration={500} className='hvr'>
+            Testimonial
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="contact"
+            smooth={true}
+            offset={-260}
+            duration={500}
+            className="btn cnt"
+          >
+            Contact Us
+          </Link>{" "}
         </li>
       </ul>
     </nav>
